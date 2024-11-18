fb1c47813e3967b9be284d981310a84bf7c28690
alexey <alexeylos2002@mail.ru>
2024-11-18 23:49:18 +0300

    first com

---
+++ person1/file_oper.py
@@ -0,0 +1,10 @@
+def write_file(filename, content):
+    with open(filename, 'w') as file:
+        file.write(content)
+    print(f"Content written to {filename}")
+
+def read_file(filename, content):
+    try:
+        with open(filename, 'r') as file:
+            return file.read()
+    except f"File {filename} not found."
